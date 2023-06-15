0cb9a4eec561ff503a78d3f93e4e0f363a2aca8d
Felipe Warchelski <fedororitos@gmail.com>
2023-06-15 15:50:43 -0300

    Tabuleiro Commit

--- src/br/com/unicuritiba/Main.java
+++ src/br/com/unicuritiba/Main.java
@@ -14,16 +14,16 @@ public class Main {
         System.out.println("Bem vindo ao Batalha Naval");
         System.out.println("O jogo consiste em uma batalha, onde você alocará navios em seu mapa e seu oponente fará o mesmo.");
         System.out.println("vence aquele que destruir a frota inimiga primeiro, mas claro nenhum dos dois sabe onde estão os navios do oponente");
-
-
-
-        System.out.println("Digite a coluna que deseja atacar: (1-10)");
-        int coluna = scanner.nextInt();
-        System.out.println("Digite a fileira que deseja atacar: (1-10)");
-        int fileira = scanner.nextInt();
         
         Tabuleiro tabuleiro = new Tabuleiro();
         tabuleiro.DesenhaTabuleiro();
+        System.out.println("");
+        
+        System.out.println("Digite o valor para atacar um espaço: ");
+        float jogada = scanner.nextInt();
+        
+        
+        
         scanner.close();
 
 	}
--- src/br/com/unicuritiba/model/Tabuleiro.java
+++ src/br/com/unicuritiba/model/Tabuleiro.java
@@ -2,12 +2,11 @@ package br.com.unicuritiba.model;
 
 public class Tabuleiro {
 
-    private int tamanho[][];
     private String simbolo;
   
     public void DesenhaTabuleiro(){
-    for (int fileira=0; fileira<10; fileira++){
-        for (int coluna=0; coluna<10; coluna++){
+    for (int fileira=0; fileira<0.9; fileira += 0.1){
+        for (int coluna=0; coluna<0.9; coluna += 0.1){
             System.out.print("|" + fileira + "." + coluna + "|");
         }
         System.out.println("");
