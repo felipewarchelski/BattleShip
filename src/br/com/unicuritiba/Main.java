f4eb95f538b74e1f085adb8006a09a68013ba7a2
Igor Ricardo <igor.ricardo1500@gmail.com>
2023-06-15 22:22:25 -0300

    Modificações significativas

--- src/br/com/unicuritiba/Main.java
+++ src/br/com/unicuritiba/Main.java
@@ -9,23 +9,83 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+		Random random = new Random();
 
-        System.out.println("Bem vindo ao Batalha Naval");
-        System.out.println("O jogo consiste em uma batalha, onde você alocará navios em seu mapa e seu oponente fará o mesmo.");
-        System.out.println("vence aquele que destruir a frota inimiga primeiro, mas claro nenhum dos dois sabe onde estão os navios do oponente");
-        
-        Tabuleiro tabuleiro = new Tabuleiro();
-        tabuleiro.DesenhaTabuleiro();
-        System.out.println("");
-        
-        System.out.println("Digite o valor para atacar um espaço: ");
-        float jogada = scanner.nextInt();
-        
-        
-        
-        scanner.close();
+		String[][] matriz = new String[10][10];
 
+		for (int i = 0; i < 10; i++) {
+			for (int ind = 0; ind < 10; ind++) {
+				matriz[i][ind] = Integer.toString(i) + Integer.toString(ind);
+			}
+		}
+
+		System.out.println("Bem Vindo ao Extreme BattleShip");
+		System.out.println("Vamos iniciar a organização da sua frota");
+		System.out.println();
+
+		for (int i = 0; i < 10; i++) {
+			for (int ind = 0; ind < 10; ind++) {
+				System.out.print(" " + matriz[i][ind]);
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		System.out.println("Digite a posição inicial do Navio de tamanho 3 (horizontal):");
+		String n3 = scanner.nextLine();
+
+		boolean encontrouPosicao = false;
+		boolean colocouCerto = false;
+		while (!colocouCerto) {
+			for (int i = 0; i < 10; i++) {
+				for (int ind = 0; ind < 10; ind++) {
+					String comparacao = matriz[i][ind];
+					if (n3.equals(comparacao)) {
+						if (ind + 2 < 10) {
+							matriz[i][ind] = "NV";
+							matriz[i][ind + 1] = "NV";
+							matriz[i][ind + 2] = "NV";
+							encontrouPosicao = true;
+							colocouCerto = true;
+							break;
+						} else {
+							System.out.println("Posição inválida para o navio de tamanho 3.");
+							System.out.print("Tente outra posição: ");
+							n3 = scanner.nextLine();
+						}
+					}
+				}
+				if (encontrouPosicao) {
+					break;
+				}
+			}
+		}
+
+		if (!encontrouPosicao) {
+			System.out.println("Posição não encontrada no tabuleiro.");
+		}
+
+		for (int i = 0; i < 10; i++) {
+			for (int ind = 0; ind < 10; ind++) {
+				System.out.print(" " + matriz[i][ind]);
+			}
+			System.out.println();
+		}
+		
+		
+
+		/*atirar(int linha, int coluna) {
+	        if (tabuleiro[linha][coluna] == NAVIO) {
+	            System.out.println("Você acertou um navio!");
+	            tabuleiro[linha][coluna] = ATINGIDO;
+	        } else if (tabuleiro[linha][coluna] == ATINGIDO) {
+	            System.out.println("Você já atingiu essa posição antes.");
+	        } else {
+	            System.out.println("Você errou o tiro.");*/
+		
+		
+		
+		scanner.close();
 	}
 
 }
