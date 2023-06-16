a12577b4a30f1916cad3dc931f29416a9a60fba5
Felipe Warchelski <fedororitos@gmail.com>
2023-06-16 00:54:37 -0300

    comitu

--- src/br/com/unicuritiba/Main.java
+++ src/br/com/unicuritiba/Main.java
@@ -30,29 +30,57 @@ public class Main {
         }
 
         System.out.println();
+        System.out.println("Escolha a orientação do navio(Vertical = [V] ou Horizontal = [H]):");
+        String orientacao = scanner.nextLine();
         System.out.println("Digite a posição inicial do Navio de tamanho 3 (horizontal):");
         String n3 = scanner.nextLine();
         while (Integer.parseInt(n3) > 99 || Integer.parseInt(n3) < 0 || n3.length() != 2) {
             System.out.println("Valor Inválido");
-            System.out.println("Digite a posição inicial do Navio de tamanho 3 (horizontal):");
+            System.out.println("Digite a posição inicial do Navio de tamanho 3:");
             n3 = scanner.nextLine();
         }
 
+        // POSICIONAR NAVIO HORIZONTAL
         boolean encontrouPosicao = false;
-        while (!encontrouPosicao) {
-            for (int fileira = 0; fileira < 10; fileira++) {
-                for (int coluna = 0; coluna < 10; coluna++) {
-                    String comparacao = matriz[fileira][coluna];
-                    if (n3.equals(comparacao)) {
-                        if (coluna + 1 < 10 && coluna - 1 > -1) {
-                            matriz[fileira][coluna] = "NN";
-                            matriz[fileira][coluna - 1] = "NN";
-                            matriz[fileira][coluna + 1] = "NN";
-                            encontrouPosicao = true;
-                        } else {
-                            System.out.println("Posição sem espaço para o navio de tamanho 3.");
-                            System.out.print("Tente outra posição: ");
-                            n3 = scanner.nextLine();
+        if (orientacao.equals("H") || orientacao.equals("h")) {
+            while (!encontrouPosicao) {
+                for (int fileira = 0; fileira < 10; fileira++) {
+                    for (int coluna = 0; coluna < 10; coluna++) {
+                        String comparacao = matriz[fileira][coluna];
+                        if (n3.equals(comparacao)) {
+                            if (coluna + 1 < 10 && coluna - 1 > -1) {
+                                matriz[fileira][coluna] = "NV";
+                                matriz[fileira][coluna - 1] = "NV";
+                                matriz[fileira][coluna + 1] = "NV";
+                                encontrouPosicao = true;
+                            } else {
+                                System.out.println("Posição sem espaço para o navio de tamanho 3.");
+                                System.out.print("Tente outra posição: ");
+                                n3 = scanner.nextLine();
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        //POSICIONAR NAVIO VERTICAL
+        else {
+            while (!encontrouPosicao) {
+                for (int fileira = 0; fileira < 10; fileira++) {
+                    for (int coluna = 0; coluna < 10; coluna++) {
+                        String comparacao = matriz[fileira][coluna];
+                        if (n3.equals(comparacao)) {
+                            if (coluna + 1 < 10 && coluna - 1 > -1) {
+                                matriz[fileira][coluna] = "NV";
+                                matriz[fileira - 1][coluna] = "NV";
+                                matriz[fileira + 1][coluna] = "NV";
+                                encontrouPosicao = true;
+                            } else {
+                                System.out.println("Posição sem espaço para o navio de tamanho 3.");
+                                System.out.print("Tente outra posição: ");
+                                n3 = scanner.nextLine();
+                            }
                         }
                     }
                 }
@@ -69,43 +97,29 @@ public class Main {
             }
             System.out.println();
         }
-		
         System.out.println("");
-        System.out.println(" Navio posicionado!");
+        System.out.println("Navio posicionado!");
 
-		System.out.println("Vamos testar sua pontaria");
-		System.out.println("Digite a linha do alvo");
-		String tiroL = scanner.nextLine();
-		System.out.println("Digite a coluna do alvo");
-		String tiroC = scanner.nextLine();
-		
-		
-		if (matriz[Integer.valueOf(tiroL)][Integer.valueOf(tiroC)] == "NV") {
-					System.out.println("Você acertou um tiro");
-					matriz[Integer.valueOf(tiroL)][Integer.valueOf(tiroC)] = "XX";
-				}
-		else {
-			System.out.println("Água");
-		}
+        // ATIRAR
+        System.out.println("Vamos testar sua pontaria");
+        System.out.println("Digite a linha do alvo");
+        String tiroL = scanner.nextLine();
+        System.out.println("Digite a coluna do alvo");
+        String tiroC = scanner.nextLine();
 
-		for (int i = 0; i < 10; i++) {
-			for (int ind = 0; ind < 10; ind++) {
-				System.out.print(" " + matriz[i][ind]);
-			}
-			System.out.println();
-		}
+        if ("NV".equals(matriz[Integer.valueOf(tiroL)][Integer.valueOf(tiroC)])) {
+            System.out.println("Você acertou um tiro");
+            matriz[Integer.valueOf(tiroL)][Integer.valueOf(tiroC)] = "XX";
+        } else {
+            System.out.println("Água");
+        }
 
-
-        /*
-         * atirar(int linha, int coluna) {
-         * if (tabuleiro[linha][coluna] == NAVIO) {
-         * System.out.println("Você acertou um navio!");
-         * tabuleiro[linha][coluna] = ATINGIDO;
-         * } else if (tabuleiro[linha][coluna] == ATINGIDO) {
-         * System.out.println("Você já atingiu essa posição antes.");
-         * } else {
-         * System.out.println("Você errou o tiro.");
-         */
+        for (int i = 0; i < 10; i++) {
+            for (int ind = 0; ind < 10; ind++) {
+                System.out.print(" " + matriz[i][ind]);
+            }
+            System.out.println();
+        }
 
         scanner.close();
     }
